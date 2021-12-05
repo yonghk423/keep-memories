e0eb12701c7ba06bf828a626df86c8f6ebe68dd8
yonghk423 <yonghk423423@gmail.com>
2021-12-05 13:39:28 +0900

    router setting

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import ShoppingCart from './page/ShoppingCart';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
     <Header/>    
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
+      <Route path="Main" element={<ShoppingCart/>}/>
     </Routes>
     </BrowserRouter>      
     </>
