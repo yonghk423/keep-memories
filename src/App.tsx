b2c78310aa1f0c6275b05363abe8a4bd14462c53
yonghk423 <yonghk423423@gmail.com>
2021-12-23 12:06:26 +0900

    header cartitems count function

--- src/App.tsx
+++ src/App.tsx
@@ -16,42 +16,10 @@ function App() {
   const [Items, setItems] = useState<DataList['Items']>(initialState.items);
   const [CartItems, setCartItems] = useState<CartDataList['CartItems']>(initialState.cartItems);  
   
-  // const AddCart = ( ItemId:number ) => {    
-  //   const Find = CartItems.filter((ele:any):boolean => (ele.ItemId === ItemId))[0]
-  //   if(Find) {
-  //     console.log('기존 리스트와 일치하는 상품')
-  //     SettingQuantity( Find.quantity + 1, ItemId )
-  //   }
-  //   else {
-  //     console.log('새로운 상품 추가')
-  //     setCartItems( 
-  //       [...CartItems,
-  //         {
-  //           ItemId,
-  //           quantity: 1
-  //         }
-  //       ])
-  //   }
-  // }
-  // const SettingQuantity = ( quantity: number, ItemId: number) => {
-  //   const Find:number = CartItems.filter((ele:any):boolean => (ele.ItemId === ItemId))[0]
-  //   const Idx:number = CartItems.indexOf(Find)
-  //   const CartItemsSetting:object = { 
-  //     ItemId, 
-  //     quantity 
-  //   }  
-  
-  //   setCartItems([
-  //     ...CartItems.slice(0, Idx),
-  //     CartItemsSetting,
-  //     ...CartItems.slice(Idx + 1)
-  //   ])
-  // }
-  
   return (
     <>    
     <BrowserRouter>
-    <Header CartItems={CartItems}/>    
+    <Header/>    
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
       <Route path="Main" element={
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Header.css'
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 
-const Header = ( CartItems:any ) => (
-         <div className="HeaderBox">
+export interface ItemReducer {
+        ItemReducer : object[]
+}
+
+const Header = ( ) => {
+    const state:any = useSelector<ItemReducer>(state=> state.ItemReducer);
+    console.log(state);
+    console.log(state.cartItems.length)
+    return (
+        <div className="HeaderBox">
              <div className="MarketName">YongHee Market</div>
                <div className="ItemBox">
                  <Link to="/"><div className="ItemList">상품 리스트</div></Link>
-                 <Link to="Main"><div className="ItemCount">장바구니 {CartItems.CartItems.length}개</div></Link>
+                 <Link to="Main"><div className="ItemCount">장바구니 {state.cartItems.length}개</div></Link>
                </div>             
-         </div>   
-    );
+         </div>
+        )            
+       };
 
 export default Header;
