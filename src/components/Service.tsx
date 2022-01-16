8d31ebfc4b0bb4d15225c4494a8a176cd064a6db
yonghk423 <yonghk423423@gmail.com>
2022-01-16 23:37:57 +0900

    service box

---
+++ src/components/Service.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Service.css';
+
+
+const Service = () => {
+    return (
+        <div className='serviceBox'>
+            <img className='imgBox' src="" alt=""></img>
+            <div className='infoBox'></div>
+        </div>
+    )
+}
+
+export default Service;
--- src/page/LandingPage.tsx
+++ src/page/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from '../components/Main';
+import Service from '../components/Service';
 
 const landingPage = () => {
 
     return (
-        <>          
+        <>
+          <Service/>          
           <Main/>
         </>
     )
