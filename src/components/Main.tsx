4dc24f5b135796e6e609fa33fef8095c3570fb61
yonghk423 <yonghk423423@gmail.com>
2022-01-22 20:18:12 +0900

    scss

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -42,17 +42,15 @@ const Main = () => {
     return (  
           <div className="slider">          
             {items.map((item) => (
+            <>
             <div className="slider-thumb" key={item.id}>  
-              {/* <img className="ImgBox" src={item.img} alt=""/> */}
               <label htmlFor={item.name} className="ItemInfo">{item.name}</label>
-              <div className='div'>
-                <input id={item.name} type="radio" name="slides" />
-              </div>
-              {/* <span className='Text'>{item.text}</span> */}
-              {/* <div className='BtnBox'>
-              <button className="ItemBtn" onClick={() => AddCartSetting(item.id)}>장바구니 추가</button>
-              </div> */}
-            </div>              
+            </div>  
+            <div className='sliderBox'>
+              <input id={item.name} type="radio" name="slides" className='input' />
+              {/* <img id={item.name} className="ImgBox" src={item.img} alt=""/>                             */}
+            </div>
+            </>            
             ))}                       
           </div>
     )
