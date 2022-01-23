92b54f9f4e84108f876470ab8fd719e6cf3ec142
yonghk423 <yonghk423423@gmail.com>
2022-01-23 22:02:31 +0900

    scss

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { AddCart } from '../actions';
 import { useSelector, useDispatch } from 'react-redux';
 import './Main.scss';
+import { initialState } from '../asset/data'
+console.log(initialState)
 
 export interface ItemReducer {
         ItemReducer : Array<object>
@@ -23,6 +25,7 @@ export interface DataSetting {
 
 const Main = () => {
   const state:any = useSelector<ItemReducer>(state=> state.ItemReducer);
+  console.log(state);
   const dispatch = useDispatch();
   const {items, cartItems}:DataSetting = state;
   console.log({items, cartItems})
@@ -39,20 +42,24 @@ const Main = () => {
     }
   }
 
-    return (  
-          <div className="slider">          
+  const SelectedImg = (itemId: number) => {
+    //사진을 눌렀을 때 
+    console.log(itemId)
+  }
+
+    return (
+        <div className='total'>  
+          <div className="mainBox">
+            <img className="bigImg" src={items[0].img} alt=""/>          
             {items.map((item) => (
-            <>
-            <div className="slider-thumb" key={item.id}>  
-              <label htmlFor={item.name} className="ItemInfo">{item.name}</label>
-            </div>  
-            <div className='sliderBox'>
-              <input id={item.name} type="radio" name="slides" className='input' />
-              {/* <img id={item.name} className="ImgBox" src={item.img} alt=""/>                             */}
-            </div>
-            </>            
+            <div className='thumImg' key={item.id}>            
+              <img src={item.img} alt=""
+              onClick={() => SelectedImg(item.id)}
+              ></img>                
+            </div>          
             ))}                       
           </div>
+        </div>    
     )
 }
 export default Main;
