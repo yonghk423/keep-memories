be50c76fbb5b38f02bd9e777fc99107857586f44
yonghk423 <yonghk423423@gmail.com>
2022-01-25 12:02:21 +0900

    scss

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { AddCart } from '../actions';
-import { useSelector, useDispatch } from 'react-redux';
+// import { AddCart } from '../actions';
+// import { useSelector, useDispatch } from 'react-redux';
 import './Main.scss';
 import { initialState } from '../asset/data'
 
@@ -11,43 +11,43 @@ export interface DataList {
   setThumImg:any;
 }
 
-export interface ItemReducer {
-        ItemReducer : Array<object>
-}
+// export interface ItemReducer {
+//         ItemReducer : Array<object>
+// }
 
-export interface DataSetting {  
-  items: [{
-    price:number;
-    id:number;
-    img:string;
-    name:string;
-    text:string;
-  }]
-  cartItems: [{
-    quantity:number;
-    itemId:number;
-  }]; 
-}
+// export interface DataSetting {  
+//   items: [{
+//     price:number;
+//     id:number;
+//     img:string;
+//     name:string;
+//     text:string;
+//   }]
+//   cartItems: [{
+//     quantity:number;
+//     itemId:number;
+//   }]; 
+// }
 
 const Main = () => {
   const [fullImg, setFullImg] = useState<DataList['fullImg']>(initialState.items[0].img);
-  const [thumImg, setThumImg] = useState<DataList['thumImg']>(initialState.items)
-  const state:any = useSelector<ItemReducer>(state=> state.ItemReducer);
-  const dispatch = useDispatch();
-  const {items, cartItems}:DataSetting = state;
-  console.log({items, cartItems})
+  const [thumImg] = useState<DataList['thumImg']>(initialState.items)
+  // const state:any = useSelector<ItemReducer>(state=> state.ItemReducer);
+  // const dispatch = useDispatch();
+  // const {items, cartItems}:DataSetting = state;
+  // console.log({items, cartItems})
   
-  const AddCartSetting = ( itemId:number ) => {    
-    const find = cartItems.filter((ele):boolean => (ele.itemId === itemId))[0]
-    if(!find) {
-      console.log(find);
-      console.log('새로운 상품 추가')
-      dispatch(AddCart(itemId))
-    }
-    else {
-      console.log('기존 리스트와 일치하는 상품')
-    }
-  }
+  // const AddCartSetting = ( itemId:number ) => {    
+  //   const find = cartItems.filter((ele):boolean => (ele.itemId === itemId))[0]
+  //   if(!find) {
+  //     console.log(find);
+  //     console.log('새로운 상품 추가')
+  //     dispatch(AddCart(itemId))
+  //   }
+  //   else {
+  //     console.log('기존 리스트와 일치하는 상품')
+  //   }
+  // }
   const onImgChange = (eleImg:any) => {
    setFullImg(eleImg);
   }
@@ -55,11 +55,11 @@ const Main = () => {
     return (
         <div className='total'>  
           <div className="fullImgBox">
-            <img src={fullImg} alt=""></img>
+            <img className='fullImg' src={fullImg} alt=""></img>
           </div>
           <div className='thumImgBox'>
             {thumImg.map((ele:any)=> (
-              <img className='thumImg' src={ele.img} alt="" onClick={() => onImgChange(ele.img)}></img>
+              <img className='thumImg' key={ele.id} src={ele.img} alt="" onClick={() => onImgChange(ele.img)}></img>
             ))}
           </div>
         </div>    
