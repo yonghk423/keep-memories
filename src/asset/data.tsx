513fc5b88f611e8c0d5b576b383bbba35254fdbd
yonghk423 <yonghk423423@gmail.com>
2022-02-02 14:52:33 +0900

    scss

--- src/asset/data.tsx
+++ src/asset/data.tsx
@@ -4,7 +4,7 @@ export const initialState =
     {
       "id": 1,
       "name": "맥북",
-      "img": "https://unsplash.it/1600/880/?random",
+      "img": "https://images.velog.io/images/yonghk423/post/00995394-70d4-480e-bf98-e699dffda06f/red16.jpeg",
       "price": 9900,
       "text": '스타벅스 출입증',
       "textBox": [{
@@ -16,7 +16,7 @@ export const initialState =
     {
       "id": 2,
       "name": "아이폰",
-      "img": "https://unsplash.it/1600/780/?random",
+      "img": "https://media.vlpt.us/images/yonghk423/post/1b0c1f1b-72bf-4864-b6c4-1be205b1abfd/1.jpeg",
       "price": 12000,
       "text": '시에라 블루로 더 멋진 감성을',
       "textBox": [{
@@ -28,7 +28,7 @@ export const initialState =
     {
       "id": 3,
       "name": "에어팟",
-      "img": "https://unsplash.it/1600/680/?random",
+      "img": "https://images.velog.io/images/yonghk423/post/fdcbfd65-45c3-4372-8324-f2b3ce6f1d36/5.jpeg",
       "price": 2900,
       "text": '노이즈 캔슬링으로 더 강한 몰입',
       "textBox": [{
@@ -40,7 +40,7 @@ export const initialState =
     {
       "id": 4,
       "name": "아이패드",
-      "img": "https://unsplash.it/1600/580/?random",
+      "img": "https://media.vlpt.us/images/yonghk423/post/138d7bdc-d719-410d-ada3-a4c2aa53af07/3.jpeg",
       "price": 4900,
       "text": '노트북과 스마트폰의 장점',
       "textBox": [{
@@ -53,7 +53,7 @@ export const initialState =
     {
       "id": 5,
       "name": "갤럭시 탭",
-      "img": "https://unsplash.it/1600/480/?random",
+      "img": "https://media.vlpt.us/images/yonghk423/post/466c5c0b-dec3-46b9-b67a-ff60e3e94b13/4.jpeg",
       "price": 3900,
       "text": '성능과 효율의 극대화',
       "textBox": [{
@@ -66,7 +66,7 @@ export const initialState =
     {
       "id": 6,
       "name": "Z플립",
-      "img": "https://unsplash.it/1600/380/?random",
+      "img": "https://images.velog.io/images/yonghk423/post/a6040640-2b42-428f-91ff-ed83013b8c8f/2.webp",
       "price": 4900,
       "text": '갤럭시에도 감성이 생겼다',
       "textBox": [{
