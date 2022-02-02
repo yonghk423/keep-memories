5c24a8a2db50c788ac8643d25ff644cdd164ec5b
yonghk423 <yonghk423423@gmail.com>
2022-02-02 16:24:39 +0900

    scss

--- src/asset/data.tsx
+++ src/asset/data.tsx
@@ -3,75 +3,75 @@ export const initialState =
   "items": [
     {
       "id": 1,
-      "name": "맥북",
+      "name": "leisure time",
       "img": "https://images.velog.io/images/yonghk423/post/00995394-70d4-480e-bf98-e699dffda06f/red16.jpeg",
       "price": 9900,
-      "text": '스타벅스 출입증',
+      "text": 'Live life to the fullest',
       "textBox": [{
                     id: 1,
-                    name: "맥북",
+                    name: "leisure time",
                     text: '',
                   }]  
     },
     {
       "id": 2,
-      "name": "아이폰",
+      "name": "beautiful architecture",
       "img": "https://media.vlpt.us/images/yonghk423/post/1b0c1f1b-72bf-4864-b6c4-1be205b1abfd/1.jpeg",
       "price": 12000,
-      "text": '시에라 블루로 더 멋진 감성을',
+      "text": 'Behind the cloud is the sun still shining',
       "textBox": [{
                     id: 1,
-                    name: "아이폰",
+                    name: "beautiful architecture",
                     text: '',
                   }]
     },
     {
       "id": 3,
-      "name": "에어팟",
+      "name": "spring spirit",
       "img": "https://images.velog.io/images/yonghk423/post/fdcbfd65-45c3-4372-8324-f2b3ce6f1d36/5.jpeg",
       "price": 2900,
-      "text": '노이즈 캔슬링으로 더 강한 몰입',
+      "text": 'Life is really simple',
       "textBox": [{
                     id: 1,                    
-                    name: "에어팟",
+                    name: "spring spirit",
                     text: '',
                   }]
     },
     {
       "id": 4,
-      "name": "아이패드",
+      "name": "the aesthetics of slowness",
       "img": "https://media.vlpt.us/images/yonghk423/post/138d7bdc-d719-410d-ada3-a4c2aa53af07/3.jpeg",
       "price": 4900,
       "text": '노트북과 스마트폰의 장점',
       "textBox": [{
                     id: 1,
-                    name: "아애패드",
+                    name: "the aesthetics of slowness",
                     text: '',
                   }]
     },
     
     {
       "id": 5,
-      "name": "갤럭시 탭",
+      "name": "gives peace of mind",
       "img": "https://media.vlpt.us/images/yonghk423/post/466c5c0b-dec3-46b9-b67a-ff60e3e94b13/4.jpeg",
       "price": 3900,
-      "text": '성능과 효율의 극대화',
+      "text": 'Time you enjoy wasting is not wasted time',
       "textBox": [{
                     id: 1,
-                    name: "갤럭시 탭",
+                    name: "gives peace of mind",
                     text: '',
                   }]
     },
 
     {
       "id": 6,
-      "name": "Z플립",
+      "name": "harmony of colors",
       "img": "https://images.velog.io/images/yonghk423/post/a6040640-2b42-428f-91ff-ed83013b8c8f/2.webp",
       "price": 4900,
-      "text": '갤럭시에도 감성이 생겼다',
+      "text": 'Better the last smile than the first laughter',
       "textBox": [{
                     id: 1,
-                    name: "Z플립",
+                    name: "harmony of colors",
                     text: '',
                   }]
     }    
