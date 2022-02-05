0e45a47aa7597e87b58ce29b90233baddf86dde8
yonghk423 <yonghk423423@gmail.com>
2022-02-05 13:10:11 +0900

    clean

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-// import { AddCart } from '../actions';
-// import { useSelector, useDispatch } from 'react-redux';
 import './Main.scss';
 import { initialState } from '../asset/data'
 import { Link } from 'react-router-dom';
@@ -12,50 +10,12 @@ export interface DataList {
   setThumImg:any;
 }
 
-// console.log(initialState.items[0])
-
-// export interface ItemReducer {
-//         ItemReducer : Array<object>
-// }
-
-// export interface DataSetting {  
-//   items: [{
-//     price:number;
-//     id:number;
-//     img:string;
-//     name:string;
-//     text:string;
-//   }]
-//   cartItems: [{
-//     quantity:number;
-//     itemId:number;
-//   }]; 
-// }
-
 const Main = () => {
   const [fullImg, setFullImg] = useState<DataList['fullImg']>(initialState.items[0]);
-  const [thumImg] = useState<DataList['thumImg']>(initialState.items)
-
-  
+  const [thumImg] = useState<DataList['thumImg']>(initialState.items)  
   const imgData = fullImg.img  
   const idData = (fullImg.id)
-  // console.log(idData);
-  // const state:any = useSelector<ItemReducer>(state=> state.ItemReducer);
-  // const dispatch = useDispatch();
-  // const {items, cartItems}:DataSetting = state;
-  // console.log({items, cartItems})
   
-  // const AddCartSetting = ( itemId:number ) => {    
-  //   const find = cartItems.filter((ele):boolean => (ele.itemId === itemId))[0]
-  //   if(!find) {
-  //     console.log(find);
-  //     console.log('새로운 상품 추가')
-  //     dispatch(AddCart(itemId))
-  //   }
-  //   else {
-  //     console.log('기존 리스트와 일치하는 상품')
-  //   }
-  // }
   const onImgChange = (ele:any) => {
     console.log(ele);
     setFullImg(ele);
