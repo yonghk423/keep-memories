3cb74e94f8abbfdc60f0206eaf47611fae04e8e0
yonghk423 <yonghk423423@gmail.com>
2022-02-08 12:53:38 +0900

    info

--- src/components/InfoUpload.tsx
+++ src/components/InfoUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useDebugValue, useState } from 'react';
 
 const InfoUpload = () => {
   const [infoData, setInfoData] = useState({
@@ -16,10 +16,20 @@ const InfoUpload = () => {
   const {id, name, price, text, textBox} = infoData;
   console.log({id, name, price, textBox})
    
+  const onChange = (e:any) => {
+    const {name, price, text} = e.target;
+    setInfoData({
+      ...infoData,
+      [name]: name,
+      [price]: price,
+      [text]: text,
+    })
+  }
+
   return (
         <div>
             <form className='submitInfo'>
-                <input name='name' value={name}/>
+                <input name='name' value={name} onChange={onChange}/>
                 <input name='price' value={price}/>                                
                 <input name='text' value={text}/>              
             </form>
