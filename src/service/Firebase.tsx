470e30d8539cd1b9ea70d5c0cad5c57bd930832f
yonghk423 <yonghk423423@gmail.com>
2022-02-09 12:35:48 +0900

    firebase

---
+++ src/service/Firebase.tsx
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHhlvaYULf4enbeubFsf6t_x1FCRSQcpI",
+  authDomain: "photo-shop-7e516.firebaseapp.com",
+  projectId: "photo-shop-7e516",
+  storageBucket: "photo-shop-7e516.appspot.com",
+  messagingSenderId: "185347764417",
+  appId: "1:185347764417:web:560fb3601b57e7c2eb2440",
+  measurementId: "G-4J7C4V3TWV"
+};
+
+export default firebase.initializeApp(firebaseConfig);
