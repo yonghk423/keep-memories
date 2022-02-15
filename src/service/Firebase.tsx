2c20b76f22ea9bbf8cffb53f580ecba765ece5b7
yonghk423 <yonghk423423@gmail.com>
2022-02-15 13:26:59 +0900

    firebase storage

--- src/service/Firebase.tsx
+++ src/service/Firebase.tsx
@@ -1,14 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAb65AZfOcElbRIrXuYbkqOucWJ5_Lh2Dk",
-  authDomain: "photoshop-fe98b.firebaseapp.com",
-  projectId: "photoshop-fe98b",
-  storageBucket: "photoshop-fe98b.appspot.com",
-  messagingSenderId: "705971571653",
-  appId: "1:705971571653:web:ad7d3426d9765b18d9d9ac",
-  measurementId: "G-FQZTB533JP"
+const firebaseConfig = {  
+  storageBucket: "photoshop-fe98b.appspot.com",  
 };
 
 export const app = initializeApp(firebaseConfig);
