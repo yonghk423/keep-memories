4ca3dd55006138ae4560b92913eef58f5e989f95
yonghk423 <yonghk423423@gmail.com>
2022-03-01 12:48:29 +0900

    server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
-const http = require('http');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import 'express-async-errors';
 
-const server = http.createServer((req, res) => {
-    console.log('test')
-});
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'))
 
-server.listen(8080);
+app.use((req, res, next) => {
+    res.sendStatus(404);
+})
+
+app.use((error, req, res, next)=> {
+    console.error(error);
+    res.sendStatus(500);
+})
+app.listen(8080);
