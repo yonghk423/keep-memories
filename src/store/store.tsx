45de3c20ecc574ccf164ec4b75c781ba6e64ba9e
yonghk423 <yonghk423423@gmail.com>
2022-03-04 13:25:43 +0900

    redux saga

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index'
 import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer , applyMiddleware(ReduxThunk))
+const store = createStore(rootReducer , composeWithDevTools(applyMiddleware(ReduxThunk)))
 
 export default store;
