435739d8eb16f9a4e3fa51a72de9bc201e2bffbf
yonghk423 <yonghk423423@gmail.com>
2022-04-02 17:19:38 +0900

    client

--- src/api/getData.tsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const getData = async () => {
-  const response = await axios.get('http://localhost:8080/initialState');
-  console.log(response);
-  return response.data;
-};
